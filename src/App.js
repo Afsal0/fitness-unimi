3b3c252d9c1e177ef7939273b619b1191246440e
Ahamed Abdullah <ahamed.abdullah00772@gmail.com>
2021-06-27 23:13:33 +0530

    abd is great

--- src/App.js
+++ src/App.js
@@ -5,80 +5,63 @@ import Course from "./Pages/course/course";
 import Contact from "./Pages/contact/contact";
 import Video from "./Pages/videoconf/video";
 import Logout from "./Pages/logout/logout";
-import { createContext,useEffect } from "react";
+import { createContext, useEffect } from "react";
 import { useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  useHistory
 } from "react-router-dom";
 
 export const Context = createContext('');
 
 function App() {
-  const [user,setuser]=useState('false')
-  const history = useHistory();
-  const [mail,setmail]=useState(()=> 'afsal@uni.com');
-  
- 
-  const [pwd,setpwd]=useState('');
+  const [user, setuser] = useState('false')
+  const [mail, setmail] = useState(() => 'afsal@uni.com');
+  const [pwd, setpwd] = useState('');
 
-  // const routeValidation = function () {
-  //   const t = localStorage.getItem('localvar');
-  //   debugger
-  //    console.log(t)
-  //   if (t === null) {
-  //     console.log("fuck")
-  //     history.push('/login');
-  //   }
-  // }
-
-
-  let t;
-  useEffect(()=>{
-    t = localStorage.getItem('localvar');
-  },[])
-  
-  
-  
-
+  const routeValidation = function () {
+    const t = JSON.parse(localStorage.getItem('localvar'))
+    if (t === null || t.pwd === '') {
+      window.location.href = 'http://localhost:3000/login';
+    }
+  }
 
   return (
     <Router>
-    <Switch>
-      <Route path="/login">
-         <Login  mail={mail} setmail={setmail} pwd={pwd} setpwd={setpwd} user={user} setuser={setuser}/>
-      </Route>
-      <Context.Provider value={mail}>
-      <Route  path="/home">
-        {t ? <Home />:<Login />}
-      </Route>
-      <Route path="/signup">
-       <Signup/>
-      </Route>
-      <Route path="/course">
-          <Course />
-      </Route>
-      <Route path="/contact">
-      <Contact />
-      </Route>
-      <Route path="/video">
-       <Video />
-      </Route>
-      <Route path="/logout">
-        <Logout/>
-      </Route>
+      <Switch>
+        <Route path="/login">
+          <Login mail={mail} setmail={setmail} pwd={pwd} setpwd={setpwd} user={user} setuser={setuser} />
+        </Route>
+        <Context.Provider value={mail}>
+          <Route path="/home">
+            <Home routeValidation={routeValidation} />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/course">
+            <Course />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/video">
+            <Video />
+          </Route>
+          <Route path="/logout">
+            <Logout />
+          </Route>
 
-      </Context.Provider>
-     
-     
+        </Context.Provider>
 
-      
-    </Switch>
+
+
+
+      </Switch>
     </Router>
-  
-   
+
+
   );
 }
 
--- src/Pages/home/home.jsx
+++ src/Pages/home/home.jsx
@@ -3,56 +3,60 @@ import NavBar from '../../navbar/navbar'
 import "./home.scss"
 import { Context } from '../../App'
 import { Redirect } from 'react-router'
-import { useReducer } from 'react'
+import { useReducer, useEffect } from 'react'
 import moment from 'moment'
 import { motion } from 'framer-motion'
 
-export default function Home() {
-    const date=moment().format("YYYY-MM-DD");
+export default function Home({ routeValidation }) {
+    const date = moment().format("YYYY-MM-DD");
 
-    const mail= useContext(Context)
+    const mail = useContext(Context)
 
-    const initialcount=0;
+    const initialcount = 0;
 
-    const reducer =(state,action)=>
-    {
-        switch(action)
-        {
+    const reducer = (state, action) => {
+        switch (action) {
             case 'INCREMENT':
-                return state+1;
+                return state + 1;
             case 'DECREMENT':
-                return state-1;
-            default :
-            return state
+                return state - 1;
+            default:
+                return state
         }
 
     }
-    const [state,dispatch]=useReducer(reducer,initialcount)
+    const [state, dispatch] = useReducer(reducer, initialcount)
 
-    const Increment=()=>{
+    const Increment = () => {
         dispatch('INCREMENT')
     }
 
-    const Decrement=()=>{
+    const Decrement = () => {
         dispatch('DECREMENT')
     }
-     
-   
+
+    routeValidation()
+
+    useEffect(() => {
+        console.log(JSON.parse(localStorage.getItem('localvar')))
+    }, [])
+
+
     return (
         <>
-        <div>
-            <NavBar />
-        </div>
-        <div className="home">
-            <h1>Welcome Home  ,   {mail}</h1>
-            <h1>Date:{date}</h1>
-            <h1>{state}</h1>
-        <button onClick={Increment}>Increment</button>
-        <button onClick={Decrement}>Decrement</button>
-       
-        
-        </div>
-       
+            <div>
+                <NavBar />
+            </div>
+            <div className="home">
+                <h1>Welcome Home  ,   {mail}</h1>
+                <h1>Date:{date}</h1>
+                <h1>{state}</h1>
+                <button onClick={Increment}>Increment</button>
+                <button onClick={Decrement}>Decrement</button>
+
+
+            </div>
+
         </>
     )
 }
